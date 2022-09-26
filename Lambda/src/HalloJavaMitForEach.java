e656f613d5a58c1f116e2f5518011cba53bb3cc3
FelixBerger1 <BergerF190284@sus.htl-grieskirchen.at>
2022-09-26 09:32:09 +0200

    Commit 2

--- Lambda/src/HalloJavaMitForEach.java
+++ Lambda/src/HalloJavaMitForEach.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 public class HalloJavaMitForEach {
-    private ArrayList<String> list = new ArrayList<>();
+    private ArrayList<String> list = (ArrayList<String>) Arrays.asList("Hello","World","!");
+
 
     public void forEach() {
-        list.add("Hello");
-        list.add("World");
         for (String s : list) {
             System.out.println(s);
         }
@@ -14,4 +15,11 @@ public class HalloJavaMitForEach {
     public void forEachLambda() {
         list.forEach(System.out::println);
     }
+
+    public void Consumer()
+    {
+        Consumer<String> consumer = (String s) ->System.out.println(s);
+    }
+
+
 }
