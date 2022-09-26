00f2d7bf12b0691e1b522aeedf861fb3dd396bb9
FelixBerger1 <BergerF190284@sus.htl-grieskirchen.at>
2022-09-26 09:24:01 +0200

    Commit 1

--- Lambda/src/HalloJavaMitForEach.java
+++ Lambda/src/HalloJavaMitForEach.java
@@ -3,4 +3,15 @@ import java.util.ArrayList;
 public class HalloJavaMitForEach {
     private ArrayList<String> list = new ArrayList<>();
 
+    public void forEach() {
+        list.add("Hello");
+        list.add("World");
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    public void forEachLambda() {
+        list.forEach(System.out::println);
+    }
 }
