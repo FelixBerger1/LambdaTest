abf38e3a02e2427c04516692ec72467e9fdec4ff
FelixBerger1 <BergerF190284@sus.htl-grieskirchen.at>
2022-09-26 14:54:29 +0200

    Commit 8

--- Lambda/src/Main.java
+++ Lambda/src/Main.java
@@ -5,7 +5,28 @@ public class Main {
     private static NumberTester numberTester = new NumberTester("Tests");
 
     public static void main(String[] args) throws IOException {
-        numberTester.setOddEvenTester();
+        numberTester.setOddEvenTester(number -> number % 2 == 0);
+        numberTester.setPrimeTester(number -> {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i < number; i++)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        });
+        numberTester.setPalindromeTester(number -> {
+            int palindrome = number;
+            int reverse = 0;
+
+            while (palindrome != 0) {
+                int remainder = palindrome % 10;
+                reverse = reverse * 10 + remainder;
+                palindrome = palindrome / 10;
+            }
+            return number == reverse;
+        });
         numberTester.testFile();
 
 
--- Lambda/src/NumberTester.java
+++ Lambda/src/NumberTester.java
@@ -30,25 +30,42 @@ public class NumberTester {
 
     public void testFile() {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));) {
-            String s = bufferedReader.readLine();
-            String[] parts = s.split(" ");
-            int i = Integer.parseInt(parts[0]);
-            int j = Integer.parseInt(parts[1]);
-            switch (i) {
-                case 1:
-                    System.out.println("OddEvenTest");
-                    oddTester.testNumber(j);
-                    break;
-                case 2:
-                    System.out.println("PrimeTester");
-                    primeTester.testNumber(j);
-                    break;
-                case 3:
-                    System.out.println("PalindromTester");
-                    palindromeTester.testNumber(j);
-                    break;
-            }
+            int b = Integer.parseInt(bufferedReader.readLine());
+            int c = 0;
+            while (c < b) {
+                c++;
+                String s = bufferedReader.readLine();
+                String[] parts = s.split(" ");
+                int i = Integer.parseInt(parts[0]);
+                int j = Integer.parseInt(parts[1]);
+                switch (i) {
+                    case 1:
+                        System.out.println("OddEvenTest");
+                        if (oddTester.testNumber(j)) {
+                            System.out.println("Even");
+                        } else {
+                            System.out.println("Odd");
+                        }
 
+                        break;
+                    case 2:
+                        System.out.println("PrimeTester");
+                        if (primeTester.testNumber(j)) {
+                            System.out.println("Ist Prim");
+                        } else {
+                            System.out.println("Ist Nicht Prim");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("PalindromTester");
+                        if (palindromeTester.testNumber(j)) {
+                            System.out.println("Ist ein Palindrom");
+                        } else {
+                            System.out.println("Ist kein Palindrom");
+                        }
+                        break;
+                }
+            }
         } catch (Exception e) {
             System.out.println("Fehler");
         }
