d2c0e1f623b2109d7c405c7f75dbfe3898bf4f07
FelixBerger1 <BergerF190284@sus.htl-grieskirchen.at>
2022-09-26 15:10:10 +0200

    Commit 9

--- Lambda/src/Number.java
+++ Lambda/src/Number.java
@@ -1,2 +1,13 @@
 public class Number {
+    private double a;
+    private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
 }
